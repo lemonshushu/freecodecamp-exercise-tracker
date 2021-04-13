acc2b29381991762c1dca782225a145e1463bcae
lemonshushu <ifnt7luv@gmail.com>
2021-04-14 08:40:50 +0900

    Final commit

--- myApp.js
+++ myApp.js
@@ -5,18 +5,17 @@ const moment = require('moment');
 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 
-const exerciseSchema = new Schema({
-  description: {type: String, required: true},
-  duration: {type: Number, required: true},
-  date: Date
-});
+const exerciseSchema = new Schema();
+
+const logSchema = new Schema({
+  log: [Object]
+})
 
 const userSchema = new Schema({
-  username: {type: String, required: true},
-  log: [exerciseSchema]
+  username: {type: String, required: true}
 });
 
-const Exercise = mongoose.model('Exercise', exerciseSchema);
+const Log = mongoose.model('Log', logSchema);
 const User = mongoose.model('User', userSchema);
 
 const createAndSaveUser = (username, done) => {
@@ -37,32 +36,45 @@ const getAllUsers = (done) => {
 const addExercise = (userId, exercise, done) => {
   let date = new Date(exercise.date)
   if (isNaN(date.getTime())) {
+    console.log('isNan');
     date = Date.now();
   }
-  const newExercise = new Exercise({...exercise, date: date});
-  User.findById(userId, (err, data) => {
-    if (err) return console.error(err);
-    data.log.push(newExercise);
-    data.save((err, data) => {
-      if (err) return console.error(err);
-      done(null, data);
+  const newExercise = {...exercise, date: date};
+  User.findById(userId, (err, foundUser) => {
+    if (err) console.error(err);
+    if (!foundUser) done(null, {error: 'No User Found!'});
+    Log.findById(userId, (err, foundLog) => {
+      let currLog;
+      if (!foundLog) {
+        currLog = new Log({_id: userId, log: []})
+      } else {
+        currLog = foundLog;
+      }
+      currLog.log.push(newExercise);
+      currLog.save((err, savedLog) => {
+        if (err) console.log(err);
+        done(null, {...foundUser._doc, ...newExercise});
+      });
     });
-  });
+  })
 };
 
 const getExerciseLog = (options, done) => {
   const {userId, from, to, limit} = options;
-  User.findById(userId, (err, data) => {
+  User.findById(userId, (err, foundUser) => {
     if (err) console.error(err);
-    let result = Object.assign({}, data._doc);
-    if (from && to) {
-      result['log'] = result['log'].filter(exercise => exercise.date >= new Date(from) && exercise.date <= new Date(to));
-    }
-    if (limit) {
-      result['log'] = result['log'].slice(undefined, limit);
-    }
-    result.count = result['log'].length;
-    done(null, result);
+    let result = Object.assign({}, foundUser._doc);
+    Log.findById(userId, (err, foundLog) => {
+      if (err) console.error(err);
+      if (from && to) {
+        result.log = foundLog.log.filter(exercise => exercise.date >= new Date(from) && exercise.date <= new Date(to));
+      }
+      if (limit) {
+        result.log = result.log.slice(undefined, limit);
+      }
+      result.count = result.log.length;
+      done(null, result);
+    });
   });
 };
 
